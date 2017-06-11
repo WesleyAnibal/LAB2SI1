c876d70ce95eab82080e4ff0e3cc23b628b24c6c
wesleyanibal <wesley.anibal@ccc.ufcg.edu.br>
2017-06-11 15:25:00 -0300

    opção de series não encontrada e de series sem imagens adicionada

---
+++ seriesController.js
@@ -0,0 +1,29 @@
+angular.module("series", []);
+angular.module("series").controller("seriesController",function($scope, $http, seriesAPI, listAPI){
+    $scope.series = [];
+    $scope.getNome = function(nome){
+      $scope.series = [];
+        seriesAPI.getSeries(nome).then(function(promise){
+          if(promise.data.Response != 'False'){
+            $scope.series = listAPI.pesquisa(promise.data.Search,4);
+          }else{
+            $scope.series.push(['N/A']);
+          }
+        }, function(error){
+          console.log(error);
+        });
+    }
+    $scope.watchList = [];
+    $scope.queroAssistir = function(id){
+      var filme = seriesAPI.getSerie(id).then(function(resolve){
+        listAPI.adicionaWL(resolve.data, $scope.watchList);
+      },function(){
+
+      });
+    };
+    $scope.watchedList = [];
+    $scope.assistidos = function(filme){
+      listAPI.watchedList.push(filme, $scope.watchedList);
+    };
+
+});
